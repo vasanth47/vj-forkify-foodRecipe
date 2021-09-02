19b9cb83dd02b51b3dd04a5c60a4323486333589
vasanth47 <vasanthakumarit47@gmail.com>
2021-09-02 22:39:01 +0530

    First commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ee0dc0e9-d884-4890-8223-e34222a2d471';
+export const MODAL_CLOSE_SEC = 2.5;
